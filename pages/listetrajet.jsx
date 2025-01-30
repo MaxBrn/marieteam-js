8852a30a3aeee7c212fef0df14834803c1ff9e8f
Maxime Brunin <maxime_brn@outlook.fr>
2025-01-30 11:30:51 +0100

    modif style

--- pages/listetrajet.jsx
+++ pages/listetrajet.jsx
@@ -285,7 +285,7 @@ export default function ListeTrajet() {
   };
 
   return (
-    <div className="pt-16 w-9/12 mx-auto">
+    <div className="pt-16 pb-8 w-9/12 mx-auto">
       <form className="w-1/2 m-auto mb-10" onSubmit={handleSubmit}>
       {/* Liste déroulante pour sélectionner un secteur */}
         <select
